bf207dd533ef2e7a6fbcb245cbfe4d926db01758
zhurbey <kamisama42@gmail.com>
2016-01-06 21:26:19 +0700

    made it so that the image in the event window is cropped

--- KKEvents/Chat.swift
+++ KKEvents/Chat.swift
@@ -3,7 +3,20 @@
 3.12 2:10pm
 Hey man, just made this file for chat purposes and notes. Here we can write what we've done or changed.
 
+TO DO
+adjust constraints to work on all displays
+loading screen
+refresh button
+adjust event picture
+resize logos
+pull to refresh?
 
+push notifications
+populate lists
+
+
+PROBLEMS
+app crashes if it tries to pull a non-existent image
 
 
 
