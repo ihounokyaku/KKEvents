25568375517871ce56e3dc314d90c3534184f3cc
Dylan Southard <kamisama42@gmail.com>
2017-09-27 22:22:05 +0700

    commit

---
+++ KKEvents/MainViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MainViewController.swift
+//  KKNightlife
+//
+//  Created by Dylan Southard on 2017/09/27.
+//  Copyright © 2017 Dylan. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    var navDelegate:BottomNavViewController!
+}
